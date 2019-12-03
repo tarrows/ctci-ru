07bfc44c2b19d7173fdcb81b60c7ead691ad36f5
c10t <c10t@fa.ke>
2019-12-03 23:58:29 +0900

    a bit edit

--- src/chapter_01.rs
+++ src/chapter_01.rs
@@ -1,9 +1,11 @@
-pub fn q_01() {}
+pub fn q_01(input: &str) -> String {
+  format!("{}", input)
+}
 
 #[cfg(test)]
 mod tests {
   #[test]
   fn q_01() {
-    assert_eq!(2 + 2, 4);
+    assert_eq!("apple", "apple");
   }
 }
