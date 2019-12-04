a0bc3634350576a6000ca857c5ae0fd21deaed7a
c10t <c10t@fa.ke>
2019-12-04 23:52:22 +0900

    solve 1.1

--- src/chapter_01.rs
+++ src/chapter_01.rs
@@ -1,5 +1,17 @@
-pub fn q01(input: &str) -> String {
-  String::from(input)
+use std::collections::HashSet;
+
+pub fn q01_a(input: &str) -> bool {
+  let mut charset = HashSet::new();
+
+  for c in input.chars() {
+    if charset.contains(&c) {
+      return false;
+    }
+
+    charset.insert(c);
+  }
+
+  true
 }
 
 #[cfg(test)]
@@ -7,7 +19,8 @@ mod tests {
   use super::*;
 
   #[test]
-  fn q_01_test() {
-    assert_eq!(q01("apple"), "apple");
+  fn q01_test() {
+    assert_eq!(q01_a("apple"), false);
+    assert_eq!(q01_a("orange"), true);
   }
 }
